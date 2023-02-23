483b2da2173890564a91e104f9cf8e13c80b5268
gbadesha <gurpreetmba@gmail.com>
2023-02-23 12:01:54 -0500

    First commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+int i=0;
+        do
+    {System.out.println("Enter 1 for addition");
+        System.out.println("Enter 2 for subtraction");
+        System.out.println("Enter 3 for multiplication");
+        System.out.println("Enter 4 for division");
+        System.out.println("Enter 0 for exit");
+        Scanner sc=new Scanner(System.in);
+        i = sc.nextInt();} while(i!=0);
+        //int[]=
+
+    }
+    }
