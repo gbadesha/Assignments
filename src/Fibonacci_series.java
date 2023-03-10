8b9bfd3c90e88bbd8a76a525d0245cf9ebfc0a44
gbadesha <124325446+gbadesha@users.noreply.github.com>
2023-03-09 21:31:49 -0500

    Update Fibonacci_series.java
    changes by Gurpreet Badesha

--- src/Fibonacci_series.java
+++ src/Fibonacci_series.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Fibonacci_series {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of fibonacci series");
+        System.out.println("Enter the length of the fibonacci series");
         int l= sc.nextInt();
         int a=0, b=0, c=1;
         for (int i=0;i<=l;i++)
